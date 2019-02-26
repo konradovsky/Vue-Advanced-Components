e9a92ddef95cd180099c40f0dc12dbddedd18be7
Konrad Barrek <kbarrek@divante.pl>
2019-02-26 15:37:00 +0100

    Datepicker without vue logic

--- 03_Wrapping External Libraries as Vue Components/src/App.vue
+++ 03_Wrapping External Libraries as Vue Components/src/App.vue
@@ -4,6 +4,7 @@
       <div class="card">
         <label class="block">
           <span class="form-label mb-2">Select a date</span>
+          <input class="form-input" type="text" id="datepicker" placeholder="YYYY-MM-DD">
         </label>
       </div>
     </div>
@@ -11,8 +12,16 @@
 </template>
 
 <script>
-
+import Pikaday from 'pikaday';
+import 'pikaday/css/pikaday.css'
 export default {
+  mounted(){
+    const picker = new Pikaday({
+      field: document.getElementById('datepicker'),
+      format: "YYYY-MM-DD"
+    })
+  }
+
 }
 </script>
 
