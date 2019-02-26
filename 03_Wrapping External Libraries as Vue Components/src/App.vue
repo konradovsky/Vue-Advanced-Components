27bf2dde7eda7e717e621569375ee948daa0e932
Konrad Barrek <kbarrek@divante.pl>
2019-02-26 15:25:47 +0100

    Initial commit

---
+++ 03_Wrapping External Libraries as Vue Components/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-sm mx-auto">
+      <div class="card">
+        <label class="block">
+          <span class="form-label mb-2">Select a date</span>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style src="./assets/app.css"/>
