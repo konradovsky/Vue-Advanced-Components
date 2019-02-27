406175178628fdc4688fa1cbcc053cde3c284cef
Efix <konrad.trade@gmail.com>
2019-02-27 12:26:30 +0100

    portals and its usage

---
+++ 06_Encapsulating External Behavior Portals/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="min-h-screen bg-grey-darker p-8">
+      <div class="max-w-sm mx-auto">
+        <user-settings-form :account-id="accountId"></user-settings-form>
+      </div>
+    </div>
+
+    <portal-target name="modals"></portal-target>
+  </div>
+</template>
+
+<script>
+import UserSettingsForm from './components/UserSettingsForm.vue'
+
+export default {
+  components: {
+    UserSettingsForm,
+  },
+  data() {
+    return {
+      accountId: 3,
+    }
+  }
+}
+</script>
+
+<style src="./assets/app.css"/>
---
+++ 06_Encapsulating External Behavior Portals/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import PortalVue from "portal-vue"
+
+Vue.config.productionTip = false
+
+Vue.use(PortalVue)
+
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
