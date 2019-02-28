a39f41dc0f7a212bb2a70dcc81265d1cb67fab5f
Efix <konrad.trade@gmail.com>
2019-02-28 14:16:48 +0100

    Merging calsses

---
+++ src/components/ToggleInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="toggle" role="checkbox" tabindex="0"
+    @click="toggle"
+    @keydown.space.prevent="toggle"
+    :aria-checked="toggled.toString()"
+  ></span>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'toggled'
+  },
+  props: ["toggled"],
+  methods: {
+    toggle() {
+      this.$emit("input", !this.toggled)
+    }
+  }
+}
+</script>
