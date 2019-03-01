ffc20d4db9295f79cd6f46c64dedab9834ab6fd2
Efix <konrad.trade@gmail.com>
2019-03-01 13:23:54 +0100

    Work in progress part 1 composition

---
+++ 10_Extending Components Using Composition/src/components/DeleteAccountButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button type="button" 
+    @click="confirmDeleteModalOpen = true"
+  >
+  <slot></slot>
+    <portal to="modals">
+      <confirm-delete-modal
+        :show="confirmDeleteModalOpen"
+        @close="confirmDeleteModalOpen = false"
+        :account-id="accountId"
+      />
+    </portal>
+  </button>
+</template>
+
+<script>
+import ConfirmDeleteModal from "./ConfirmDeleteModal.vue"
+
+export default {
+  components: {
+    ConfirmDeleteModal
+  },
+  props: ["accountId"],
+  data() {
+    return {
+      confirmDeleteModalOpen: false
+    }
+  }
+}
+</script>
